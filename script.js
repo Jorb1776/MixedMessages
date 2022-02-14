9ef57c602cc3ae70a96f15eaaf55b5aea575f7d3
JorB <jordan.b@marineeast.com>
2022-02-14 12:54:57 -0600

    Add factory; not implemented

--- script.js
+++ script.js
@@ -65,3 +65,51 @@ for (let i = 0; i < npcArray.length; i++) {
       npcArray[i].npcDisposition
   );
 }
+
+// const npc = {
+//   _chars: {
+//     names: [],
+//     genders: [],
+//     dispositions: [],
+//   },
+
+//   get names() {
+//     return this._chars.names;
+//   },
+//   set names(data){
+//     this._chars.names = data;
+//   },
+
+//   get genders(){
+//     return this._chars.genders;
+//   },
+//   set genders(){
+//     this._chars.genders = data;
+//   },
+
+//   get dispositions(){
+//     return this._chars.dispositions;
+//   },
+//   set dispositions(){
+//     this._chars.dispositions = data;
+//   },
+
+//   get chars(){
+//     return {
+//       names: this.names,
+//       genders: this.genders,
+//       dispositions: this.dispositions
+//     };
+//   },
+
+//   addNpc(npcName, npcGender, npcDisposition){
+//     const = npc = {
+//       npcName: npcName,
+//       npcGender: npcGender,
+//       npcDisposition: npcDisposition,
+//     };
+//     return this._chars[npcName].push(npc);
+//   },
+
+//   getRandomNpc(npcName)
+// };
